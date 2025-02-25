fa7c97df91894b85d61f2fbc1baa0ef1eb121a09
waimea-mlimrungroj <mlimrungroj@waimea.school.nz>
2025-02-25 08:28:06 +0700

    finish

--- src/Main.kt
+++ src/Main.kt
@@ -38,16 +38,33 @@ fun main() {
 
     // Sort the list
     snacks.sort()
-    println(snacks)
+    println("Sorted list of snack: $snacks")
 
     // get a random
     val item = snacks.random()
-    println(item)
+    println("Random item: $item")
 
     // loop throung the list
     for (snacks in snacks) {
-        println(snacks)
+        println("Loop: $snacks")
 
     }
+    // loop using index
+    for (i in 0..snacks.size) {
+        println("$i: ${snacks[i]}")
+    }
+    // loop getting index and values
+    for ((i, snacks) in snacks.withIndex()) {
+        println("$i: $snacks")
+    }
+
+    // searching for item
+    println("Pockey is in list: ${snacks.contains("Pockey")}")
+    println("Horse is in list: ${snacks.contains("Horse")}")
+
+
+    // getting index of an item
+    println("Pockey index: ${snacks.indexOf("Pockey")}")
+    println("Horse index: ${snacks.indexOf("Horse")}")
 }
 
