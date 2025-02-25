894907d4102baefd3481495a58998d46cbcbf01e
waimea-mlimrungroj <mlimrungroj@waimea.school.nz>
2025-02-25 07:24:13 +0700

    new stuff about snack listing

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,50 @@
  */
 
 fun main() {
-    println("Hello World!")
+    println("Kotlin MutableList!")
+    println()
+
+    // Create our list
+    val snacks = mutableListOf<String>()
+
+    //Show it
+    println(snacks)
+
+    // Add in somme things...
+    snacks.add("Seaweed")
+    snacks.add("Pringles")
+    snacks.add("Lays")
+
+    println(snacks)
+
+    // Show specific item
+    println(snacks[0])
+    // prinlin(snacks[3]) would be out of bound
+
+    // Change value
+    snacks[1] = "Pockey"
+
+    println(snacks)
+
+    // Remove an item
+    snacks.removeAt(0)
+    println(snacks)
+
+    // Add in new items in specific places
+    snacks.add(1, " Fruit burst")
+
+    // Sort the list
+    snacks.sort()
+    println(snacks)
+
+    // get a random
+    val item = snacks.random()
+    println(item)
+
+    // loop throung the list
+    for (snacks in snacks) {
+        println(snacks)
+
+    }
 }
 
